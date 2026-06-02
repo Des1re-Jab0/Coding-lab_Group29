0d3f408904e5de669a1760b72e07a9daf023185a
yann <y.sheja@alustudent.com>
2026-06-02 14:21:25 +0200

    Adding security

---
+++ hospital_admin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+secure_data() {
+	echo ""
+
+	echo "===Securing active_logs directory ==="
+
+	chmod 700 active_logs 
+
+	echo "Permission applied. Current state of active_logs:"
+
+	ls -ld active_logs
+}

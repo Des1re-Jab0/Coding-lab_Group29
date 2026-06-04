1c60597b58ea226738129ff50b1af004a5e5358d
abenigne <coderb@Bithiah.localdomain>
2026-06-04 11:24:30 +0200

    fixing an issue in initialising a system

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
-initialize_system() {
-
-    if [ ! -d active_logs ]; then
-        echo "Creating active_logs directory..."
-        mkdir active_logs
-    fi
-
-    if [ ! -d archived_logs ]; then
-        echo "Creating archived_logs directory..."
-        mkdir archived_logs
-    fi
-
-    if [ ! -d reports ]; then
-        echo "Creating reports directory..."
-        mkdir reports
-    fi
+initialize_system()
+{
+    for dir in active_logs archived_logs reports; do
+        if [ -d "$dir" ]; then
+            echo "$dir directory already exists."
+        else
+            echo "Creating $dir directory..."
+            mkdir "$dir"
+        fi
+    done
 }
-

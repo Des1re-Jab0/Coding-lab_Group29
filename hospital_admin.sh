40955bdf7240946f715d92af678a4500cbd3707a
abenigne <coderb@Bithiah.localdomain>
2026-06-04 11:24:30 +0200

    fixing an issue in initialising a system

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
+
 #!/bin/bash
+=======
+>>>>>>> 1c60597 (fixing an issue in initialising a system)
 initialize_system()
 {
     for dir in active_logs archived_logs reports; do
@@ -10,30 +12,6 @@ initialize_system()
             mkdir "$dir"
         fi
     done
-=======
-<<<<<<< HEAD
-
-#!/bin/bash
-
-=======
-#!/bin/bash
->>>>>>> c1f09ed (Add initialize_system function)
-initialize_system() {
-
-    if [ ! -d active_logs ]; then
-        echo "Creating active_logs directory..."
-        mkdir active_logs
-    fi
-
-    if [ ! -d archived_logs ]; then
-        echo "Creating archived_logs directory..."
-        mkdir archived_logs
-    fi
-
-    if [ ! -d reports ]; then
-        echo "Creating reports directory..."
-        mkdir reports
-    fi
->>>>>>> 96a81eb (final step)
 }
 initialize_system
+

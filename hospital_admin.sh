79a0fbfe2417364e3d96538c335a1910f19a3f9d
abenigne <coderb@Bithiah.localdomain>
2026-06-04 12:19:42 +0200

    Add initialize_system function

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 initialize_system()
 {
@@ -9,5 +10,30 @@ initialize_system()
             mkdir "$dir"
         fi
     done
+=======
+<<<<<<< HEAD
+
+#!/bin/bash
+
+=======
+#!/bin/bash
+>>>>>>> c1f09ed (Add initialize_system function)
+initialize_system() {
+
+    if [ ! -d active_logs ]; then
+        echo "Creating active_logs directory..."
+        mkdir active_logs
+    fi
+
+    if [ ! -d archived_logs ]; then
+        echo "Creating archived_logs directory..."
+        mkdir archived_logs
+    fi
+
+    if [ ! -d reports ]; then
+        echo "Creating reports directory..."
+        mkdir reports
+    fi
+>>>>>>> 96a81eb (final step)
 }
 initialize_system
